11eeab2351290bb1ed7c89041623e7e8bf60334a
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-03-12 03:49:40 +0530

    backSpace button
    a new backSpace button is added which removes(deletes) the last digit.

--- js/main.js
+++ js/main.js
@@ -8,6 +8,9 @@ function clearDisplay() {
  display.value = "";
 }
 
+function backSpace() {
+    display.value = display.value.slice(0,-1);
+}
 function calculate() {
     try{
         display.value = eval(display.value);
